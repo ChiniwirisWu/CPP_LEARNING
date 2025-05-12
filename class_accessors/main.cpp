20d542bb67077f56628ad3c0a8b483ec8f02f3a4
gilbertowu <gilbertowuwu0@gmail.com>
2025-05-11 23:14:56 -0400

    second excercise of class acccessors

--- class_accessors/main.cpp
+++ class_accessors/main.cpp
@@ -18,15 +18,30 @@ public:
   void print() const{
     std::cout << "<" << m_x << "," << m_y << "," << m_z << ">" << std::endl;
   }
+
+  bool isEqual(Point3d& point2){
+    if(m_x == point2.m_x && m_y == point2.m_y && m_z == point2.m_z){
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 int main()
 {
-    Point3d point;
-    point.setValues(1, 2, 3);
+	Point3d point1{};
+	point1.setValues(1, 2, 3);
 
-    point.print();
-    std::cout << '\n';
+	Point3d point2{};
+	point2.setValues(1, 2, 3);
 
-    return 0;
+	std::cout << "point 1 and point 2 are" << (point1.isEqual(point2) ? "" : " not") << " equal\n";
+
+	Point3d point3{};
+	point3.setValues(3, 4, 5);
+
+	std::cout << "point 1 and point 3 are" << (point1.isEqual(point3) ? "" : " not") << " equal\n";
+
+	return 0;
 }
